8734d2a73f362b8daeae2937de9a0b25720a6ba3
Adam Basiak <basiakus@gmail.com>
2017-11-02 09:57:24 +0100

    exercise 9.1 fixed, add exercise 9.3

--- scripts.js
+++ scripts.js
@@ -1,15 +1,24 @@
 // --rozdział 9--
-// zadanie 9.1 FUNKCJE
+
+/*
+// ---zadanie 9.1 FUNKCJE---
+
 function triAngleArea(a, h) {
     if (a > 0 && h > 0) {
         return a * h / 2
+    } else if (a <= 0 || h <= 0) {
+       console.log("Podane wartości muszą być większe od 0");
     } else {
         console.log("Nieprawidłowe dane");
     }
 }
-console.log(triAngleArea(10, 6));
+console.log(triAngleArea(0, 7));
+*/
 
-// zadanie 9.2 TABLICE
+
+
+/*
+// ---zadanie 9.2 TABLICE---
 
 var femaleNames = ['Kasia', 'Basia', 'Zosia'];
 var maleNames = ['Adam', 'Krzysztof', 'Paweł'];
@@ -23,3 +32,23 @@ if (addNames.indexOf(newMan) === -1) {
 } else {
     console.log("Imię: '" + newMan + "' jest już wypisany na tablicy");
 }
+*/
+
+
+
+/*
+//---Zadanie 9.3 łańcuchy znaków / triceratops---
+
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+console.log(text);
+var dinozaur = 'triceratops';
+var bigDinozaur = dinozaur.toUpperCase();
+console.log(bigDinozaur);
+var dinoReplace = text.replace('Velociraptor', bigDinozaur);
+console.log(dinoReplace);
+console.log(dinoReplace.length/2)
+
+var shortText = dinoReplace.slice(0, 72);
+console.log(shortText);
+console.log(shortText.length);
+*/
