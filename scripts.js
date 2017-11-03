37f00b503370a067e1c64fe875cb402739d3eba0
Adam Basiak <basiakus@gmail.com>
2017-11-03 20:55:08 +0100

    update christmas tree

--- scripts.js
+++ scripts.js
@@ -55,21 +55,17 @@ console.log(shortText);
 console.log(shortText.length);
 */
 
-/*
-//---Zadanie 9.4 choinka---
-function drawTree(x) {
 
-for ( var i = 0 ; i < x ; i++) {
-   var star = '';
-  
-   
-      for ( var j = 0; j < i; j++) {
-      star += '*';
-         
-      }
-    console.log(star);
-   }
-  
+//---Zadanie 9.4 choinka---
+function drawTree(row) {
+
+    for (var i = 0; i < row; i++) {
+        var star = '';
+        for (var j = -1; j < i; j++) {
+            star += '*';
+        }
+        console.log(star);
+    }
 }
-drawTree(8);
-*/
+drawTree(4);
+
